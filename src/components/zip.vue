48ffbbe1efdddc5ed0ae92db737e1e524a652cab
MARCEL <MARCEL>
2017-10-26 00:52:08 +0100

    error message

--- src/components/zip.vue
+++ src/components/zip.vue
@@ -7,6 +7,8 @@
         </form>
       </div>
       <div class="contain">
+        
+        <p class="alert alert-danger" role="alert" v-show="error">{{error}}</p>
       <div v-for="place in places" :key="place.state">
         <ul class="list-group">
           <li class="list-group-item active">{{place['place name']}}</li>
@@ -26,7 +28,8 @@ export default {
   data () {
     return {
       current: '',
-      places:[]
+      error: '',
+      places:[],
     }
   },
   methods: {
@@ -34,9 +37,14 @@ export default {
       e.preventDefault();
       this.$http.get('http://api.zippopotam.us/us/'+ this.current)
       .then(response=>{
+        this.places='';
+        this.error = '';
         this.places = response.data.places;
         console.log(response)
         
+      })
+      .catch(() => {
+        this.error = 'Please enter a valid Zip code'
       });
     }
   }
